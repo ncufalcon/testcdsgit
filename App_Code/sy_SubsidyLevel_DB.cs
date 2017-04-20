7a7e5402f69b091af6c927b0fdede864ed656883
watermelonwang <wcc@bestitmaster.com>
2017-04-20 14:41:26 +0800

    基本設定/保險設定完成，masterpage加上保險設定連結，PACImport加上上傳檔案格式限制

---
+++ App_Code/sy_SubsidyLevel_DB.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Text;
+
+/// <summary>
+/// sy_SubsidyLevel_DB 的摘要描述
+/// </summary>
+public class sy_SubsidyLevel_DB
+{
+    public sy_SubsidyLevel_DB()
+    {
+        //
+        // TODO: 在這裡新增建構函式邏輯
+        //
+    }
+    #region 全 私用
+    string str_keyword = string.Empty;
+    #endregion
+    # region 全 公用
+    public string _str_keyword
+    {
+        set { str_keyword = value; }
+    }
+    #endregion
+
+    #region sy_SubsidyLevel 私用
+    string slGuid = string.Empty;
+    string slSubsidyCode = string.Empty;
+    string slSubsidyIdentity = string.Empty;
+    decimal slSubsidyRatio1;
+    decimal slSubsidyRatio2;
+    decimal slSubsidyRatio3;
+    string slCreatId = string.Empty;
+    string slModifyId = string.Empty;
+    string slStatus = string.Empty;
+    DateTime slCreatDate;
+    DateTime slModifyDate;
+    #endregion
+    #region sy_SubsidyLevel 公用
+    public string _slGuid
+    {
+        set { slGuid = value; }
+    }
+    public string _slSubsidyCode
+    {
+        set { slSubsidyCode = value; }
+    }
+    public string _slSubsidyIdentity
+    {
+        set { slSubsidyIdentity = value; }
+    }
+    public Decimal _slSubsidyRatio1
+    {
+        set { slSubsidyRatio1 = value; }
+    }
+    public Decimal _slSubsidyRatio2
+    {
+        set { slSubsidyRatio2 = value; }
+    }
+    public Decimal _slSubsidyRatio3
+    {
+        set { slSubsidyRatio3 = value; }
+    }
+    public string _slCreatId
+    {
+        set { slCreatId = value; }
+    }
+    public string _slModifyId
+    {
+        set { slModifyId = value; }
+    }
+    public string _slStatus
+    {
+        set { slStatus = value; }
+    }
+    public DateTime _slCreatDate
+    {
+        set { slCreatDate = value; }
+    }
+    public DateTime _slModifyDate
+    {
+        set { slModifyDate = value; }
+    }
+    #endregion
+
+    #region 撈 sy_SubsidyLevel  全/BY GUID/BY KEYWORD
+    public DataTable SelectSubsidyLevel()
+    {
+        DataTable dt = new DataTable();
+        SqlConnection thisConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnString"].ToString());
+        SqlCommand thisCommand = thisConnection.CreateCommand();
+        SqlDataAdapter oda = new SqlDataAdapter();
+        StringBuilder show_value = new StringBuilder();
+        try
+        {
+            thisConnection.Open();
+            show_value.Append(@"  
+                select * from sy_SubsidyLevel where slStatus='A'
+            ");
+            if (str_keyword != "")
+            {
+                show_value.Append(@" and (upper(slSubsidyCode) LIKE '%' + upper(@str_keyword) + '%' or upper(slSubsidyIdentity) LIKE '%' + upper(@str_keyword) + '%' )  ");
+                thisCommand.Parameters.AddWithValue("@str_keyword", str_keyword);
+            }
+            if (slGuid != "")
+            {
+                show_value.Append(@" and slGuid=@slGuid  ");
+                thisCommand.Parameters.AddWithValue("@slGuid", slGuid);
+            }
+            show_value.Append(@" order by slSubsidyCode ASC  ");
+            thisCommand.CommandType = CommandType.Text;
+            thisCommand.CommandText = show_value.ToString();
+            oda.SelectCommand = thisCommand;
+            oda.Fill(dt);
+        }
+        catch (Exception)
+        {
+            oda.Dispose();
+            thisConnection.Close();
+            thisConnection.Dispose();
+            thisCommand.Dispose();
+        }
+        finally
+        {
+            oda.Dispose();
+            thisConnection.Close();
+            thisConnection.Dispose();
+            thisCommand.Dispose();
+        }
+        return dt;
+
+    }
+    #endregion
+
+    #region 新增 sy_SubsidyLevel
+    public void InsertSubsidyLevel()
+    {
+        SqlConnection thisConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnString"].ToString());
+        SqlCommand thisCommand = thisConnection.CreateCommand();
+        SqlDataAdapter oda = new SqlDataAdapter();
+        StringBuilder show_value = new StringBuilder();
+        try
+        {
+            show_value.Append(@" 
+                insert into sy_SubsidyLevel(slGuid,slSubsidyCode,slSubsidyIdentity,slSubsidyRatio1,slSubsidyRatio2,slSubsidyRatio3,slCreatId,slModifyId,slModifyDate,slStatus) 
+                values(@slGuid,@slSubsidyCode,@slSubsidyIdentity,@slSubsidyRatio1,@slSubsidyRatio2,@slSubsidyRatio3,@slCreatId,@slModifyId,@slModifyDate,'A') 
+            ");
+
+            thisCommand.Parameters.AddWithValue("@slGuid", slGuid);
+            thisCommand.Parameters.AddWithValue("@slSubsidyCode", slSubsidyCode);
+            thisCommand.Parameters.AddWithValue("@slSubsidyIdentity", slSubsidyIdentity);
+            thisCommand.Parameters.AddWithValue("@slSubsidyRatio1", slSubsidyRatio1);
+            thisCommand.Parameters.AddWithValue("@slSubsidyRatio2", slSubsidyRatio2);
+            thisCommand.Parameters.AddWithValue("@slSubsidyRatio3", slSubsidyRatio3);
+            thisCommand.Parameters.AddWithValue("@slCreatId", slCreatId);
+            thisCommand.Parameters.AddWithValue("@slModifyId", slModifyId);
+            thisCommand.Parameters.AddWithValue("@slModifyDate", DateTime.Now);
+
+            thisCommand.CommandText = show_value.ToString();
+            thisCommand.CommandType = CommandType.Text;
+
+            thisCommand.Connection.Open();
+            thisCommand.ExecuteNonQuery();
+            thisCommand.Connection.Close();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        finally
+        {
+            oda.Dispose();
+            thisConnection.Close();
+            thisConnection.Dispose();
+            thisCommand.Dispose();
+        }
+
+    }
+    #endregion
+
+    #region 修改 sy_SubsidyLevel
+    public void UpdateSubsidyLevel()
+    {
+        SqlConnection thisConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnString"].ToString());
+        SqlCommand thisCommand = thisConnection.CreateCommand();
+        SqlDataAdapter oda = new SqlDataAdapter();
+        StringBuilder show_value = new StringBuilder();
+        try
+        {
+            show_value.Append(@" 
+                update sy_SubsidyLevel 
+                set slSubsidyCode=@slSubsidyCode,slSubsidyIdentity=@slSubsidyIdentity,
+                    slSubsidyRatio1=@slSubsidyRatio1,slSubsidyRatio2=@slSubsidyRatio2,slSubsidyRatio3=@slSubsidyRatio3,
+                    slModifyId=@slModifyId,slModifyDate=@slModifyDate
+                where slGuid=@slGuid
+            ");
+
+            thisCommand.Parameters.AddWithValue("@slGuid", slGuid);
+            thisCommand.Parameters.AddWithValue("@slSubsidyCode", slSubsidyCode);
+            thisCommand.Parameters.AddWithValue("@slSubsidyIdentity", slSubsidyIdentity);
+            thisCommand.Parameters.AddWithValue("@slSubsidyRatio1", slSubsidyRatio1);
+            thisCommand.Parameters.AddWithValue("@slSubsidyRatio2", slSubsidyRatio2);
+            thisCommand.Parameters.AddWithValue("@slSubsidyRatio3", slSubsidyRatio3);
+            thisCommand.Parameters.AddWithValue("@slModifyId", slModifyId);
+            thisCommand.Parameters.AddWithValue("@slModifyDate", DateTime.Now);
+
+            thisCommand.CommandText = show_value.ToString();
+            thisCommand.CommandType = CommandType.Text;
+
+            thisCommand.Connection.Open();
+            thisCommand.ExecuteNonQuery();
+            thisCommand.Connection.Close();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        finally
+        {
+            oda.Dispose();
+            thisConnection.Close();
+            thisConnection.Dispose();
+            thisCommand.Dispose();
+        }
+
+    }
+    #endregion
+
+    #region 刪除 sy_SubsidyLevel update giiStatus="D"
+    public void DeleteSubsidyLevel()
+    {
+        SqlConnection thisConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnString"].ToString());
+        SqlCommand thisCommand = thisConnection.CreateCommand();
+        SqlDataAdapter oda = new SqlDataAdapter();
+        StringBuilder show_value = new StringBuilder();
+        try
+        {
+            show_value.Append(@" 
+                update sy_SubsidyLevel set slStatus='D',slModifyId=@slModifyId,slModifyDate=@slModifyDate where slGuid=@slGuid
+            ");
+
+            thisCommand.Parameters.AddWithValue("@slGuid", slGuid);
+            thisCommand.Parameters.AddWithValue("@slModifyId", slModifyId);
+            thisCommand.Parameters.AddWithValue("@slModifyDate", DateTime.Now);
+
+            thisCommand.CommandText = show_value.ToString();
+            thisCommand.CommandType = CommandType.Text;
+
+            thisCommand.Connection.Open();
+            thisCommand.ExecuteNonQuery();
+            thisCommand.Connection.Close();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        finally
+        {
+            oda.Dispose();
+            thisConnection.Close();
+            thisConnection.Dispose();
+            thisCommand.Dispose();
+        }
+
+    }
+    #endregion
+
+
+    #region 檢查 sy_SubsidyLevel  giidentityCode 身分代碼是否重複
+    public DataTable SelectGroupInsuranceIdentityCode()
+    {
+        DataTable dt = new DataTable();
+        SqlConnection thisConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnString"].ToString());
+        SqlCommand thisCommand = thisConnection.CreateCommand();
+        SqlDataAdapter oda = new SqlDataAdapter();
+        StringBuilder show_value = new StringBuilder();
+        try
+        {
+            thisConnection.Open();
+            show_value.Append(@"  
+                select * from sy_SubsidyLevel where slStatus='A' 
+            ");
+            if (slSubsidyCode != "")
+            {
+                show_value.Append(@" and slSubsidyCode=@slSubsidyCode  ");
+                thisCommand.Parameters.AddWithValue("@slSubsidyCode", slSubsidyCode);
+            }
+            if (slGuid != "")
+            {
+                show_value.Append(@" and slGuid=@slGuid  ");
+                thisCommand.Parameters.AddWithValue("@slGuid", slGuid);
+            }
+            thisCommand.CommandType = CommandType.Text;
+            thisCommand.CommandText = show_value.ToString();
+            oda.SelectCommand = thisCommand;
+            oda.Fill(dt);
+        }
+        catch (Exception)
+        {
+            oda.Dispose();
+            thisConnection.Close();
+            thisConnection.Dispose();
+            thisCommand.Dispose();
+        }
+        finally
+        {
+            oda.Dispose();
+            thisConnection.Close();
+            thisConnection.Dispose();
+            thisCommand.Dispose();
+        }
+        return dt;
+
+    }
+    #endregion
+
+}
---
+++ handler/InsuranceLevelImport.ashx
@@ -0,0 +1,142 @@
+﻿<%@ WebHandler Language="C#" Class="InsuranceLevelImport" %>
+
+using System;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using FlexCel.Core;
+using FlexCel.XlsAdapter;
+using System.Configuration;
+
+public class InsuranceLevelImport : IHttpHandler {
+
+    public void ProcessRequest (HttpContext context) {
+        bool status = true;
+        HttpFileCollection uploadFiles = context.Request.Files;//檔案集合
+
+        SqlConnection oConn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnString"].ToString());
+        oConn.Open();
+        SqlCommand oCmd = new SqlCommand();
+        oCmd.Connection = oConn;
+        SqlTransaction myTrans = oConn.BeginTransaction();
+        oCmd.Transaction = myTrans;
+        string str_response = "";
+        string str_date = "";
+        try
+        {
+            for (int i = 0; i < uploadFiles.Count; i++)
+            {
+                HttpPostedFile aFile = uploadFiles[i];
+                ExcelFile Xls = new XlsFile(true);
+                string UpLoadPath = context.Server.MapPath("~/Template/" + System.IO.Path.GetFileName(aFile.FileName));
+                if (!Directory.Exists(UpLoadPath.Substring(0, UpLoadPath.LastIndexOf("\\"))))//如果上傳路徑中沒有該目錄，則自動新增
+                {
+                    Directory.CreateDirectory(UpLoadPath.Substring(0, UpLoadPath.LastIndexOf("\\")));
+                }
+                aFile.SaveAs(context.Server.MapPath("~/Template/" + System.IO.Path.GetFileName(aFile.FileName)));
+                string fileSpec = context.Server.MapPath("~/Template/" + System.IO.Path.GetFileName(aFile.FileName));
+                Xls.Open(fileSpec);
+                Xls.ActiveSheet = 1;
+                oCmd.Parameters.Add("@ilGuid", SqlDbType.NVarChar);
+                oCmd.Parameters.Add("@ilItem1", SqlDbType.Decimal);
+                oCmd.Parameters.Add("@ilItem2", SqlDbType.Decimal);
+                oCmd.Parameters.Add("@ilItem3", SqlDbType.Decimal);
+                oCmd.Parameters.Add("@ilItem4", SqlDbType.Decimal);
+                oCmd.Parameters.Add("@ilEffectiveDate", SqlDbType.NVarChar);
+                oCmd.Parameters.Add("@ilCreatId", SqlDbType.NVarChar);
+                oCmd.Parameters.Add("@ilModifyId", SqlDbType.NVarChar);
+                oCmd.Parameters.Add("@ilModifyDate", SqlDbType.DateTime);
+
+                DateTime dtDate;
+                int n;
+                int error_date = 0;
+                int error_num = 0;
+
+                //寫入資料庫
+                for (int j = 2; j <= Xls.GetRowCount(1); j++)
+                {
+                    if (!DateTime.TryParse(Xls.GetCellValue(j, 5).ToString().Trim(), out dtDate ) || Xls.GetCellValue(j, 5).ToString().Trim().Length != 10)
+                    {
+                        //不是日期格式 或 日期格式有誤
+                        error_date++;
+                    }
+                    else if(!int.TryParse(Xls.GetCellValue(j, 1).ToString().Trim(), out n) || !int.TryParse(Xls.GetCellValue(j, 2).ToString().Trim(), out n) || !int.TryParse(Xls.GetCellValue(j, 3).ToString().Trim(), out n) || !int.TryParse(Xls.GetCellValue(j, 4).ToString().Trim(), out n))
+                    {
+                        //不是數字格式
+                        error_num++;
+                    }
+                    else {
+                        if (str_date == "")
+                            str_date = Xls.GetCellValue(j, 5).ToString().Trim();
+
+                        oCmd.CommandText = @"
+                            insert into sy_InsuranceLevel(ilGuid,ilItem1,ilItem2,ilItem3,ilItem4,ilEffectiveDate,ilCreatId,ilModifyId,ilModifyDate,ilStatus)
+                            values(@ilGuid,@ilItem1,@ilItem2,@ilItem3,@ilItem4,@ilEffectiveDate,@ilCreatId,@ilModifyId,@ilModifyDate,'A')
+                        ";
+
+                        oCmd.Parameters["@ilGuid"].Value = Guid.NewGuid().ToString();
+                        oCmd.Parameters["@ilItem1"].Value = Convert.ToDecimal(Xls.GetCellValue(j, 1).ToString().Trim());
+                        oCmd.Parameters["@ilItem2"].Value = Convert.ToDecimal(Xls.GetCellValue(j, 2).ToString().Trim());
+                        oCmd.Parameters["@ilItem3"].Value = Convert.ToDecimal(Xls.GetCellValue(j, 3).ToString().Trim());
+                        oCmd.Parameters["@ilItem4"].Value = Convert.ToDecimal(Xls.GetCellValue(j, 4).ToString().Trim());
+                        oCmd.Parameters["@ilEffectiveDate"].Value = Xls.GetCellValue(j, 5).ToString().Trim();
+                        oCmd.Parameters["@ilCreatId"].Value = "王胖爺";
+                        oCmd.Parameters["@ilModifyId"].Value = "王胖爺";
+                        oCmd.Parameters["@ilModifyDate"].Value = DateTime.Now;
+                        oCmd.ExecuteNonQuery();
+                    }
+                }
+
+
+                if (error_date != 0 || error_num != 0)
+                {
+                    if (error_date!=0) {
+                        str_response += "\\n請檢查日期格式是否為yyyy/mm/dd";
+                    }
+                    if (error_num!=0) {
+                        str_response += "\\n請檢查級距是否皆為數字";
+                    }
+
+                    myTrans.Rollback();
+                }
+                else {
+                    myTrans.Commit();
+                }
+
+                File.Delete(context.Server.MapPath("~/Template/" + System.IO.Path.GetFileName(aFile.FileName)));
+            }
+        }
+        catch (Exception ex)
+        {
+            status = false;
+            myTrans.Rollback();
+        }
+        finally
+        {
+            oCmd.Connection.Close();
+            oConn.Close();
+            context.Response.ContentType = "text/html";
+            if (status == false) {
+                context.Response.Write("<script type='text/JavaScript'>parent.feedbackFun('匯入失敗，請聯絡系統管理員');</script>");
+            }
+            else{
+                if (str_response != "")
+                {
+                    context.Response.Write("<script type='text/JavaScript'>parent.feedbackFun('資料匯入失敗"+str_response+",nodate');</script>");
+                }
+                else {
+                    context.Response.Write("<script type='text/JavaScript'>parent.feedbackFun('資料匯入完成,"+str_date+"');</script>");
+                }
+            }
+
+        }
+    }
+
+    public bool IsReusable {
+        get {
+            return false;
+        }
+    }
+
+}
